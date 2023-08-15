fd3a373f0b2165f6cb62bca47b3703f397d0589e
Asad Zaidi <asad_josh20@hotmail.com>
2023-08-15 23:41:22 +0500

    Adding Guest Model

---
+++ src/main/java/com/example/demo/Model/Guest.java
@@ -0,0 +1,105 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "GUEST")
+public class Guest {
+    @Id
+    @Column(name = "GUEST_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAIL_ADDRESS")
+    private String emailAddress;
+    @Column(name = "ADDRESS")
+    private String Address;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "STATE")
+    private String state;
+    @Column(name = "PHONE_NUMBER")
+    private String phoneNumber;
+
+    public Guest() {
+    }
+
+    public Guest(long id, String firstName, String lastName,
+                 String emailAddress, String address, String country, String state, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        Address = address;
+        this.country = country;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
