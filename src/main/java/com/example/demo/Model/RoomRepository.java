c1dc4045c390ef5ba871a2639c3ee469acdce71f
Asad Zaidi <asad_josh20@hotmail.com>
2023-08-17 20:49:38 +0500

    Adding Room Repo

--- src/main/java/com/example/demo/Model/RoomRepository.java
+++ src/main/java/com/example/demo/Model/RoomRepository.java
@@ -1,4 +1,6 @@
 package com.example.demo.Model;
 
-public interface RoomRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository {
 }
