59700535bb05f210c9ce6e5d6de013bd09e191ba
Asad Zaidi <asad_josh20@hotmail.com>
2023-08-29 11:31:56 +0500

    customize the entities

--- src/main/java/com/example/demo/Model/Room.java
+++ src/main/java/com/example/demo/Model/Room.java
@@ -1,7 +1,13 @@
 package com.example.demo.Model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
 @Table(name = "ROOM")
 public class Room {
@@ -16,45 +22,5 @@ public class Room {
     @Column(name = "BED_INFO")
     private String bedInfo;
 
-    public Room() {
-    }
 
-    public Room(long id, String name, String roomNumer, String bedInfo) {
-        this.id = id;
-        this.name = name;
-        this.roomNumer = roomNumer;
-        this.bedInfo = bedInfo;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRoomNumer() {
-        return roomNumer;
-    }
-
-    public void setRoomNumer(String roomNumer) {
-        this.roomNumer = roomNumer;
-    }
-
-    public String getBedInfo() {
-        return bedInfo;
-    }
-
-    public void setBedInfo(String bedInfo) {
-        this.bedInfo = bedInfo;
-    }
 }
